02fbb4f717aeab10ae50299eeeb14eefc64a3649
hyeona <01.hyeona@gmail.com>
2026-08-12 11:19:22 +0900

    이벤트 리스너에 대한 이벤트 객체 메서드

--- c32.dom/js/study3.js
+++ c32.dom/js/study3.js
@@ -1,7 +1,9 @@
 // DOM : DOCUMENT OBJECT MODEL 문서 객체 모델 
 // 문서 컨텐츠가 모두 읽혀졌을 때 실행하는 함수
 // script 뒤에 defer 안 쓰고 이 함수들을 작성하면 됨 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{ // function 으로 쓰면 this는 document를 가리킴
+
+    console.log(this) // widndow
     // 객체나 HTML 요소의 내용 속성 스타일을 변경하는 것은 변수 자체를 바꾸는게 아니기 대문에 const를 주로 사용함 
     const button1 = document.querySelector('#button1');
     const button2 = document.querySelector('#button2');
@@ -12,6 +14,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // 해석 : 이벤트 리스너를 추가한다. (클릭했을 때, 함수를 실행)
     button1.addEventListener('click', function() {
         // 클릭
+        console.log(this); // button1
         ptag.innerHTML='안녕하십니까! 클릭을 하셨군요!';
     });
     
@@ -39,8 +42,53 @@ document.addEventListener("DOMContentLoaded", ()=>{
     button3.addEventListener('mousemove', () => {
         ptag.innerHTML='마우스 움직인땅';
     })
-});
 
+    document.addEventListener('click', (event) => { // this 가리키는 것 window
+        console.log(event.target.textContent);
+    })
+
+    
+    const btn4 = document.querySelector('#button4');
+    btn4.addEventListener('click', function(event) {
+        // 기본 기능 억제 
+        event.preventDefault();        
+    });
+
+
+    const btn5 = document.querySelectorAll('a');
+    btn5.forEach((tag)=>
+        tag.addEventListener('click', function(event)
+        {
+            event.preventDefault();
+        })
+    );
+
+    // 이벤트 전파 (자식->부모)
+    const inbox = document.querySelector('.inbox');
+    // 이벤트 전파 차단 
+    inbox.addEventListener('click', function(event) {
+        event.stopImmediatePropagation();
+        alert('안에 박스를 클릭하였당!');
+    })
+    
+    const frame = document.querySelector('.frame');
+    frame.addEventListener('click', function() {
+        alert('프레임을 클릭했당.');
+    })
+
+    const inputTag = document.querySelector('.input');
+    inputTag.addEventListener('input', function(event) {
+        console.log(event.target.value);
+    });
+
+    inputTag.addEventListener('focus', ()=>{
+        console.log('입력 시작할게융');
+    })
+
+    inputTag.addEventListener('blur', ()=> {
+        console.log('입력을 마치셨군용!');
+    })
+});
 
 
 // 마우스 이벤트
